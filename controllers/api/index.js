d9ffa5dcf8dc2c9cee5912cb5378059770e3c771
daniel <yoojeenkim@gmail.com>
2021-10-28 20:41:03 -0400

    added aws-sdk and s3, created route for upload

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const paperRoutes = require('./paper-routes');
 const userRoutes = require('./user-routes');
+const uploadRoutes = require('./upload-routes');
 
 router.use('/paper', paperRoutes);
 router.use('/user', userRoutes);
+router.use('/upload', uploadRoutes);
 
 module.exports = router;
